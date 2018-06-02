929c06c74c042d0199e099dd998062dbdf525966
juky <julian.chamale@outlook.com>
2018-06-01 18:08:36 -0600

    Building project and dist

---
+++ dist/mongo.d.ts
@@ -0,0 +1,16 @@
+import { MongoClient, FilterQuery, InsertOneWriteOpResult, InsertWriteOpResult } from "mongodb";
+export declare class Model<T extends Model<T>> {
+    static client: MongoClient;
+    static collection: string;
+    static findAll<T extends Model<T>>(this: new () => T, query: FilterQuery<T>): Promise<Array<T>>;
+    static findOne<T extends Model<T>>(this: new () => T, query: FilterQuery<T>): Promise<T>;
+    static insertOne<T extends Model<T>>(this: new () => T, document: T): Promise<InsertOneWriteOpResult>;
+    static insertMany<T extends Model<T>>(this: new () => T, documents: Array<T>): Promise<InsertWriteOpResult>;
+}
+export declare class Container {
+    private uri;
+    private client;
+    constructor(uri: string);
+    addModels(models: Array<typeof Model>): Promise<any>;
+}
+export declare const Collection: (collection: string) => (target: typeof Model) => void;
